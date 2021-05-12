383fb9c80bdb610da1f8d92869fd3f8650dbf919
Nik Field <nik@ordinaryartistservices.com>
2021-05-12 16:42:34 -0400

    init

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-/* Add JavaScript code here! */
-console.log('Hello World! You did it! Welcome to Snowpack :D');
+import React from "react";
+import ReactDOM from "react-dom";
+import { Dashboard, useUppy } from "@uppy/react";
+import Uppy from "@uppy/core";
+
+const uppy = useUppy(() => {
+  return new Uppy();
+});
+
+uppy.on("complete", (result) => {
+  console.log(
+    "Upload complete! We’ve uploaded these files:",
+    result.successful
+  );
+});
+
+ReactDOM.render(
+  <>
+    <Dashboard uppy={uppy} />
+  </>,
+  document.getElementById("root")
+);
